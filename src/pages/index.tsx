843416515b86b742b82c752fed98709cd8710511
unknown <nunojraa@hotmail.com>
2022-05-28 17:03:41 -0300

    sass e outros no projetos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
+import styles from './styles/home.module.scss'
 
 export default function Home() {
   return (
-    <div>
+    <h1 className={styles.title}>
       Hello World
-    </div>
+    </h1>
   )
 }
