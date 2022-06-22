9b0d581ce4c06f5a747cbb5c11b4b90b4248cba5
Adilson2902 <38479849+Adilson2902@users.noreply.github.com>
2022-06-22 19:50:07 -0300

    adicionado algumas coisas do stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import SubscribeButton from './components/SubscribeButton';
 import styles from './home.module.scss';
+import { stripe } from './services/stripe';
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props)
   return (
     <>
       <Head>
@@ -24,4 +27,22 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1LDbtVLrkU38SwUl9K3uH55Z", {
+    expand: ['product']
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: (price.unit_amount/100)
+  };
+
+  return {
+     props: {
+        product
+     }
+  }
 }
---
+++ src/pages/services/stripe.ts
@@ -0,0 +1,12 @@
+import Stripe from "stripe";
+
+console.log(process.env.STRIPE_API_KEY);
+export const stripe = new Stripe(
+    process.env.STRIPE_API_KEY, {
+        apiVersion: '2020-08-27',
+        appInfo: {
+            name: "ignews",
+            version: '0.1.0'
+        }
+    }
+)
