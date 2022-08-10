e3fc9ef684fc0bff4f6c6009c261f245b7709546
Adilson2902 <38479849+Adilson2902@users.noreply.github.com>
2022-08-10 19:50:30 -0300

    config fauna db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,12 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import { query as q} from "faunadb";
+
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/faunadb";
+
+
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -8,4 +15,32 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+
+      try 
+      {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),{
+              data: { email: user.email }
+            }
+          )
+        )
+  
+        return true;
+      }
+      catch
+      {
+        return false;
+      }
+
+    
+    },
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+    updateAge: 24 * 60 * 60, // 24 hours
+  }
 })
