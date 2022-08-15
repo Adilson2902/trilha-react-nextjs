5380a8c956da80aa3cbb534e25bc0bd67bce67f7
Adilson2902 <38479849+Adilson2902@users.noreply.github.com>
2022-08-14 21:50:19 -0300

    ajustes para o prismic cms

---
+++ .slicemachine/assets/slices/SliiceTest/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/SliiceTest';
+
+export default {
+  title: 'slices/SliiceTest'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Arrive","spans":[]}],"description":[{"type":"paragraph","text":"Do tempor cupidatat id ad. Commodo ipsum sit dolor sunt dolor et.","spans":[]}]},"slice_type":"sliice_test","id":"_Default"}} />
+_Default.storyName = ''
