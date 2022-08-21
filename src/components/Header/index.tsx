0716cff6a90262c71748a6c7fe51d508a8831bff
unknown <nunojraa@hotmail.com>
2022-08-21 09:46:43 -0300

    ajustes nos links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,24 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
+import ActiveLink from '../ActiveLink';
 import SignInButton from '../SignInButton';
 import styles from './styles.module.scss';
 
 const Header: React.FC = () => {
+  const { asPath } = useRouter();
+
+
   return <header className={styles.headerContainer}>
          <div className={styles.headerContent}>
              <img src='/images/logo.svg' alt='logo' />
              <nav>
-                 <a className={styles.active}>Home</a>
-                 <a className={styles.active}>Posts</a>
+                 <ActiveLink activeClassName={styles.active} href="/">
+                    <a>Home</a>
+                 </ActiveLink>
+                 <ActiveLink activeClassName={styles.active} href='/posts'>
+                    <a>Posts</a>
+                 </ActiveLink>
              </nav>
 
              <SignInButton />
